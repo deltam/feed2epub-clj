d10f23b5f075dfbd01e3456647a7b3bd7205c95a
deltam <deltam@gmail.com>
2010-08-06 05:15:24 +0900

    separate to get feed func from core.clj, -> feed.clj

--- src/feed2epub/feed.clj
+++ src/feed2epub/feed.clj
@@ -4,15 +4,15 @@
   (:use [clojure.xml :only (parse)]))
 
 (defstruct feed-tags :outer :blog-title :author :entry :title :content)
-(def rss2-tags (struct feed-tags :channel :title :dc:creator :item :title :description))
+(def rss2-tags (struct feed-tags :channel :title :dc:creator :item  :title :description))
 (def atom-tags (struct feed-tags :feed    :title :author     :entry :title :content))
 (def tagmap {:rss2 rss2-tags :atom atom-tags})
 
 
-(defn get-content-by-tag [tag xml]
+(defn get-cont [tag xml-el]
   (first
    (some #(if (= tag (:tag %)) (:content %))
-          (:content xml))))
+          (:content xml-el))))
 
 
 (defn xml-sub-seq [url tag f]
@@ -20,14 +20,18 @@
     (f x)))
 
 
-(defn find-content [url tag cont-tag]
-  (first (xml-sub-seq url tag #(get-content-by-tag cont-tag %))))
+(defn find-cont [url tag cont-tag]
+  (first (xml-sub-seq url tag #(get-cont cont-tag %))))
 
 
+(defn get-feed-meta [url]
+  {:blog-title (find-cont url :channel :title)
+   :author     (find-cont url :channel :dc:creator)})
 
 
-(defn feeds [url]
-  (let [xs (xml-seq (parse url))
-        type ((first xs) :tag)]
-    (println type)
-))
+(defn get-feeds [url]
+  (reverse
+   (xml-sub-seq url :item
+                #(let [title (get-cont :title %)
+                       text  (get-cont :description %)]
+                   (list title (str "<b>" title "</b>" text))))))
