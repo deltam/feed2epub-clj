80a81554dfe21f6e0ccf6cc9a313889aa4718157
deltam <deltam@gmail.com>
2010-08-12 21:54:48 +0900

    modified comment

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject feed2epub-clj "0.0.2-alpha"
-  :description "Convert rss2 feeds into .epub, on Clojure"
+  :description "Convert RSS2.0/Atom feeds into .epub, on Clojure"
   :url "http://github.com/deltam/feed2epub-clj"
   :main feed2epub.core
   :dependencies [[org.clojure/clojure "1.1.0"]
